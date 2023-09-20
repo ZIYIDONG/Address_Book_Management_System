5e8733803e657268de06e136719c48419916a6dc
ZIYIDONG <huuyaang@163.com>
2023-09-20 13:27:47 +0800

    add changeFunc

--- Address_Book_Management_System/main.cpp
+++ Address_Book_Management_System/main.cpp
@@ -1,10 +1,49 @@
 #include "showMenu.h"
-
-
+#include "exit.h"
+#include <iostream>
 
 int main()
 {
-	showMenu();
+	while (true) {
 
-	system("pasue");
+		showMenu();
+
+		int select = 0;
+		cout << "请输入数字选择要使用的功能:" << endl;
+		cin >> select;
+
+
+
+		switch (select) {
+			case 1:
+				//功能1
+				break;
+			case 2:
+				//功能2
+				break;
+			case 3:
+				//功能3
+				break;
+			case 4:
+				//功能4
+				break;
+			case 5:
+				//功能5
+				break;
+			case 6:
+				//功能6
+				break;
+			case 0://退出通讯录管理系统
+				cout << "欢迎下次使用!" << endl;
+				system("pause");
+				return 0;
+				break;
+		}
+	}
+
+
+
+	system("pause");
+
+	return 0;
 }
